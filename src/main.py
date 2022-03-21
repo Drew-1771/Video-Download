1e37612c17782d7f4bfc4a9420b8e79d27e867da
drewsyp <drewsyp@gmail.com>
2022-03-21 01:10:08 -0700

    dark mode

--- src/main.py
+++ src/main.py
@@ -4,22 +4,27 @@
 
 import tkinter
 import tkinter.ttk
+from turtle import down
 import download
 import fileUtilities
 
+color = "grey30"
+light_color = "grey40"
 auto_clear_url = True # global for now until i add it in settings
 
-def createDownload(window: tkinter.Tk, tab: tkinter.ttk.Frame, path: str, color: str) -> None:
+
+def createDownload(window: tkinter.Tk, tab: tkinter.ttk.Frame, path: str) -> None:
     '''
     Creates the download tab with all its buttons
     '''
     # Box for url entry
     url_txt = tkinter.Entry(tab, width=15)
+    url_txt.config(bg=light_color, fg="white")
     url_txt.place(relx=0.5, rely=0.5, relwidth=0.85, relheight=0.15, anchor=tkinter.CENTER)
-
+    
     # Status label
     status = tkinter.Label(tab, text="Waiting...")
-    status.config(bg = color, fg="white")
+    status.config(bg=light_color, fg="white")
     status.place(relx=0.5, rely=0.35, relwidth=0.5, relheight=0.15, anchor=tkinter.CENTER)
 
     # Download button
@@ -34,32 +39,37 @@ def createDownload(window: tkinter.Tk, tab: tkinter.ttk.Frame, path: str, color:
             status.config(text="Could not connect", bg= "red2")
     
     download_button = tkinter.Button(tab, text="Download Video", width=15, command=download_clicked)
+    download_button.config(bg=light_color, fg="white")
     download_button.place(relx=0.5, rely=0.65, relwidth=0.5, relheight=0.15, anchor=tkinter.CENTER)
+    
     # keybind
     window.bind("<Return>", download_clicked)
 
 
 def run():
     # get info
-    data = fileUtilities.JsonFile("data.json").read()
-    path = data["download_folder_path"]
+    #data = fileUtilities.JsonFile("data.json").read()
+    #path = data["download_folder_path"]
 
     # Set up window
     window = tkinter.Tk()
     window.title("Video Download")
     window.geometry("300x200")
-    window.config(bg="gray10")
+    window.config(bg=color)
 
     # set up tabs
     tabs = tkinter.ttk.Notebook(window)
-    download_tab = tkinter.ttk.Frame(tabs)
-    settings_tab = tkinter.ttk.Frame(tabs)
+    # tab colors
+    tkinter.ttk.Style().configure("TNotebook", background=color, foreground=light_color)
+
+    download_tab = tkinter.Frame(tabs, bg=color)
+    settings_tab = tkinter.Frame(tabs, bg=color)
     tabs.add(download_tab, text="Download")
     tabs.add(settings_tab, text="Settings")
     tabs.pack(expand = 1, fill ="both")
 
     # populate tabs
-    createDownload(window, download_tab, path, "gray9")
+    createDownload(window, download_tab, "vid")
     #createSettings(window, settings_tab)
 
     # run
