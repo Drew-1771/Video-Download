f71c9db906061883d3ad22d4547a483d05e2232f
Drew <drewsyp@gmail.com>
2022-03-28 18:40:13 -0700

    changed build to work off virtual env, compiled pyinstaller on my own comp so that windows doesn't detect this as a virus

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off
 :: /Q for quiet
 IF EXIST %cd%\Video-Downloader RMDIR /S %cd%\Video-Downloader
-pyinstaller --noconsole src/main.py
+call .\env\Scripts\activate && pyinstaller --noconsole src/main.py
+call deactivate
 rename %cd%\dist\main\main.exe Video-Download.exe
 move %cd%\dist\main %cd%\dist\src
 md %cd%\Video-Downloader
 move %cd%\dist\src %cd%\Video-Downloader
-del %cd%\main.spec
+IF EXIST %cd%\main.spec del %cd%\main.spec
 RMDIR %cd%\dist /S /Q %cd%\dist
